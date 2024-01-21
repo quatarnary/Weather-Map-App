f0e897a6648befe69148bf318507caffc73f55e8
quatarnary <contact@ilyasbugra.com>
2024-01-21 19:49:47 +0300

    Initial Commit

---
+++ Weather Map App/Weather_Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Map_AppApp.swift
+//  Weather Map App
+//
+//  Created by Bugra Aslan on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
